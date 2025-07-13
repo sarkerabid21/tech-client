6416f080fef5fffae35823c5c418168d831a2b2b
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-07-13 22:27:10 +0600

    home update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
-// import Services from './Services/Services';
-// import OurServices from './Services/OurServices';
-// import Marquee from './Marquee/Marquees';
+
 import Marquees from './Marquee/Marquees';
-// import BenifitsCard from './Benifits/BenifitsCard';
-// import BeMarchant from './BeMarchant/BeMarchant';
+
 import FeaturedProducts from './FeaturedProducts/FeaturedProducts';
 import TrendingProducts from './TrendingProducts/TrendingProducts';
 import TopContributors from './TopContributors/TopContributors';
 import Testimonials from './Testimonials/Testimonials';
-// import AnimatedStats from './AnimatedStat/AnimatedStats';
+
 
 const Home = () => {
     return (
@@ -18,14 +15,13 @@ const Home = () => {
             <Banner></Banner>
             <FeaturedProducts></FeaturedProducts>
             <TrendingProducts></TrendingProducts>
-            {/* <Services></Services> */}
+          
             <TopContributors></TopContributors>
             <Testimonials></Testimonials>
             
            
             <Marquees></Marquees>
-            {/* <BenifitsCard></BenifitsCard> */}
-            {/* <BeMarchant></BeMarchant> */}
+           
         </div>
     );
 };
